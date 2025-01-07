d45937673e48ce507de1a9e95b9136f52da04949
Samuel Williams <samuelspexy@gmail.com>
2025-01-07 15:02:37 +0200

    Global styles

---
+++ constants/styles/styles.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+const globalStyles = StyleSheet.create({
+  page: {
+    borderWidth: 1,
+    borderColor: "black",
+    flex: 1,
+    padding: 8,
+    alignItems: "center",
+  },
+});
+
+export { globalStyles };
